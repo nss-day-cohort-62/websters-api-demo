7cf5361b2b82b2d5d514215ac40fe15632398abc
Derek Malone <laxmalone@gmail.com>
2023-05-08 15:40:07 -0500

    got medical dictionary to send data and display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [definition, setDef] = useState([]);
+  const [display, setDisplay] = useState(false);
+  const key = process.env.REACT_APP_MEDICAL_DICTIONARY;
+
+  useEffect(() => {
+    fetch(
+      `https://www.dictionaryapi.com/api/v3/references/medical/json/femur?key=${key}`
+    )
+      .then((resp) => resp.json())
+      .then((data) => setDef(data));
+  }, []);
+
+  const shiftDisplay = () => setDisplay((prevState) => !prevState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {definition.map((def) => {
+        return (
+          <>
+            <h3>{def.meta.id.toUpperCase()}</h3>
+            <ul>
+              {def.shortdef.map((short) => (display ? "" : <li>{short}</li>))}
+            </ul>
+          </>
+        );
+      })}
+      <button type='button' onClick={shiftDisplay}>
+        Click me
+      </button>
     </div>
   );
 }
